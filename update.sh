ed9564666a1df5c9f83a69b894a0157144384a1c
wangbin <253498229@qq.com>
2018-07-07 18:03:33 +0800

    upgrade update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@
 cd /root/codeforfun/codeforfun-pc
 rm -rf target
 mkdir target
-cp /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-pc/target/webhooks.jar ./target
+cp /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-pc ./target
+cd /root/codeforfun/codeforfun-pc/target
 docker-compose build
 docker-compose rm -s -f
 docker-compose up -d
