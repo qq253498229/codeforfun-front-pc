16cfa4402499e0983a87b67edb933c64d3e38ef9
wangbin <253498229@qq.com>
2018-07-07 17:38:49 +0800

    polish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from './shared/layout/layout.component';
+import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', component: LayoutComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {
