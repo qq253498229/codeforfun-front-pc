325c1c1393d9f301357a09a8a86924c4107e4d25
wangbin <253498229@qq.com>
2018-07-07 18:10:12 +0800

    upgrade update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd /root/codeforfun/codeforfun-pc
-rm -rf target
-mkdir target
-cp /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-pc ./target
-cd /root/codeforfun/codeforfun-pc/target
+rm -rf codeforfun-pc
+mkdir codeforfun-pc
+cp -r /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-pc .
+cd /root/codeforfun/codeforfun-pc/codeforfun-pc
 docker-compose build
 docker-compose rm -s -f
 docker-compose up -d
