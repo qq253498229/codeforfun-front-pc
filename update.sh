af0ed18620ed1940aed0bba159073fe5737995f4
大胃王 <253498229@qq.com>
2018-07-09 14:06:29 +0800

    remove shell

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /root/codeforfun/codeforfun-pc
-rm -rf codeforfun-pc
-mkdir codeforfun-pc
-cp -r /root/codeforfun/codeforfun-jenkins/data/workspace/codeforfun-pc .
-cd /root/codeforfun/codeforfun-pc/codeforfun-pc
-docker-compose build
-docker-compose rm -s -f
-docker-compose up -d
