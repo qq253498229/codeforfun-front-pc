a6110cbfc13ac332a516c8095e7894839d92112c
大胃王 <253498229@qq.com>
2018-07-11 10:55:09 +0800

    add用户列表

--- src/app/modules/user/list/list.component.ts
+++ src/app/modules/user/list/list.component.ts
@@ -3,14 +3,18 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-user-list',
   templateUrl: './list.component.html',
-  styles: []
+  styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  test = [];
 
   constructor() {
   }
 
   ngOnInit() {
+    for (let i = 0; i < 15; i++) {
+      this.test.push(i);
+    }
   }
 
 }
